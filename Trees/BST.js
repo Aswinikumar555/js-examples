6d5eb3f9050f772942eed0b2ec9662605b44061d
aswini kumar <aswini.kumar@vedantu.com>
2023-07-25 12:32:26 +0530

    depthFirstInOrder

---
+++ Trees/BST.js
@@ -0,0 +1,74 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  berthFirstSearch() {
+    let node = null,
+      queue = [this.root],
+      values = [];
+
+    while (queue.length) {
+      node = queue.shift();
+      values.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return values;
+  }
+
+  depthFirstInOrder() {
+    let valuesList = [];
+
+    function traverse(node) {
+      valuesList.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+
+    return valuesList;
+  }
+}
+
+bTree = new BST();
+bTree.insert(10);
+bTree.insert(14);
+bTree.insert(5);
+bTree.insert(6);
+bTree.insert(18);
+bTree.insert(12);
